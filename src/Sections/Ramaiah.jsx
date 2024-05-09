11dd48b05a54ff6e58102f1517725fa9b3b13f03
Your Name <rafikadir2000@gmail.com>
2024-05-09 21:01:22 +0600

    add

--- src/Sections/Ramaiah.jsx
+++ src/Sections/Ramaiah.jsx
@@ -12,7 +12,10 @@ const Ramaiah = () => {
 
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-3 items-center">
                     <div>
-                        <img src={img1} alt=""/>
+                    <div className="relative">
+                        <div className="w-full h-full bg-orange absolute rounded-lg -right-3 -bottom-3"></div>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/-gbb5yr8s6Q?si=ZDPDHaqoejMcn8sQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className="z-1 relative" allowfullscreen></iframe>
+                    </div>
                     </div>
                     <div>
                         <img src={img2} alt="" className="lg:max-w-[300px] md:max-w-full mx-auto rounded-2xl "/>
